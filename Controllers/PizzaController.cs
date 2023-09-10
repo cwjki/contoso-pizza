0a43c1d06263ee328b9fc16eb88628bcc4c72689
cwjki <cwjki97@gmail.com>
2023-09-10 21:15:54 +0200

    created the PizzaController class structure

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,18 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+  public PizzaController() {}
+
+  // GET all action
+  // GET by id action
+  // POST action
+  // PUT action
+  // DELETE action
+}
